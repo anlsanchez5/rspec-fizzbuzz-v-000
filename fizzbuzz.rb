46c275ed8d02404d6f4f3847520ac968abb92782
Angelica Sanchez <anlsanchez@ucdavis.edu>
2018-09-21 05:58:35 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
-def fizzbuzz(integer)
-  if integer % 3 = 0 &&
-    if integer % 5 = 0
+def fizzbuzz(x)
+  if x % 3 == 0 &&
+    if x % 5 = 0
        puts "FizzBuzz"
     else
        puts "Fizz"
     end
-  elsif integer % 5 = 0
+  elsif x % 5 = 0
     puts "Buzz"
   else
     nil
